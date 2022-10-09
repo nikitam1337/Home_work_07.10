015f08036d0419297a212e4d65bc8932084787cb
nikitam1337 <nikitam.l@mail.ru>
2022-10-09 17:41:01 +0300

    Initial commit

---
+++ Task_1/HW_1.cs
@@ -0,0 +1,20 @@
+﻿/*Задача 19
+Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+является ли оно палиндромом.
+*/
+
+Console.Clear();
+
+int EnterNumber;
+while (true)
+{
+    Console.Write("Введите пятизначное число: ");
+    if (int.TryParse(Console.ReadLine(), out EnterNumber))
+    {
+        if (9999 < EnterNumber && EnterNumber < 100000)
+        break;
+    }
+    Console.WriteLine($"Ошибка ввода!");
+}
+
+Console.WriteLine("Вроде всё гуд");
