3424720ee6b140399cfe881e3dd468bf3ba6f85e
nikitam1337 <nikitam.l@mail.ru>
2022-10-09 18:37:17 +0300

    Выполнил 3-е задание

---
+++ Task_3/HW_3.cs
@@ -0,0 +1,27 @@
+﻿/*Задача 23
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+
+int N;
+while (true)
+{
+    Console.Write("Введите положительное число N: ");
+    if (int.TryParse(Console.ReadLine(), out N))
+        break;
+    Console.WriteLine($"Ошибка ввода!");
+}
+
+Console.Write($"{N} -> ");
+PrintCube(N);
+
+//////////////////////////////////
+static void PrintCube(int N)
+{
+    for (int i = 1; i < N; i++)
+    {
+        Console.Write($"{i * i * i}, ");
+    }
+    Console.Write($"{N * N * N}");
+}
