275e8e7136b32f9e6a477d03ac18599899c5de7c
nikitam1337 <nikitam.l@mail.ru>
2022-10-09 18:12:44 +0300

    Выполнил 1-е задание

--- Task_1/HW_1.cs
+++ Task_1/HW_1.cs
@@ -12,9 +12,26 @@ while (true)
     if (int.TryParse(Console.ReadLine(), out EnterNumber))
     {
         if (9999 < EnterNumber && EnterNumber < 100000)
-        break;
+            break;
+        else if (-100000 < EnterNumber && EnterNumber < -9999)
+            break;
     }
     Console.WriteLine($"Ошибка ввода!");
 }
 
-Console.WriteLine("Вроде всё гуд");
+Console.Write($"{EnterNumber} -> ");
+
+int num1 = EnterNumber / 10000;
+int num1_2 = EnterNumber / 1000;
+int num2 = num1_2 % 10;
+int num4_5 = EnterNumber % 100;
+int num4 = num4_5 / 10;
+int num5 = EnterNumber % 10;
+
+if (num1 == num5)
+{
+    if (num2 == num4)
+    Console.Write("Да");
+    else Console.Write("Нет");
+}
+else Console.Write("Нет");
